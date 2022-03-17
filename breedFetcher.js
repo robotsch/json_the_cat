1c861d8e9a2c7b61e5caa3e1071eec074313ff2d
robotsch <christianhumble22@gmail.com>
2022-03-17 16:01:15 -0400

    add test file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,13 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-const fetchBreed = function(breed) {
+const fetchBreedDescription = function(breed, cb) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
     if (error || body === '[]') {
       return console.log("Failed to get breed");
     }
-    console.log(JSON.parse(body));
+    const desc = JSON.parse(body)[0].description;
+    cb(error, desc);
   });
 };
 
-fetchBreed(args);
+module.exports = fetchBreedDescription;
